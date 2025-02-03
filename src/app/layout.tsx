522612795411c8fbc1dba078ae96049e38413fc5
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-02-03 23:30:50 +0530

    bg-fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Provider from "./providers";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Provider from './providers';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 export const metadata: Metadata = {
   title: 'TicTacToe',
-  description: "This is advance version of tic-tac-toe game. I hope you will enjoy the game!",
+  description:
+    'This is advance version of tic-tac-toe game. I hope you will enjoy the game!',
   icons: {
     icon: '/favicon.png',
   },
@@ -28,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} scrollbar-w-2 scrollbar-track-blue-lighter scrollbar-thumb-blue scrollbar-thumb-rounded bg-[#0d1116] antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} scrollbar-w-2 scrollbar-track-blue-lighter scrollbar-thumb-blue scrollbar-thumb-rounded bg-[#0d1116] text-white antialiased`}
       >
         <Provider>{children}</Provider>
       </body>
